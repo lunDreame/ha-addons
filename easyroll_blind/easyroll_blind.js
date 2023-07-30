fb06714885b7be09658ea67e4601cc1d4568fd1c
harwin1 <87955512+harwin1@users.noreply.github.com>
2023-07-31 06:52:52 +0900

    Update easyroll_blind.js

--- easyroll_blind/easyroll_blind.js
+++ easyroll_blind/easyroll_blind.js
@@ -1,63 +1,68 @@
-const axios = require("axios");
-const mqtt = require("mqtt");
-const fs = require("fs");
-const logger = require("simple-node-logger").createSimpleLogger();
-const options = require("/data/options.json");
+const axios = require('axios');
+const mqtt = require('mqtt');
+const fs = require('fs');
+const logger = require('simple-node-logger').createSimpleLogger();
+const options = require('/data/options.json');
 
-const stateUrl = "http://{}:20318/lstinfo";
-const actionUrl = "http://{}:20318/action";
+const stateUrl = 'http://{}:20318/lstinfo';
+const actionUrl = 'http://{}:20318/action';
 
 const command = {
-    OPEN: "TU",
-    CLOSE: "BD",
-    STOP: "SS"
+    OPEN: 'TU',
+    CLOSE: 'BD',
+    STOP: 'SS',
 };
 
-class EasyrollBlind {
+class SmartBlind {
     constructor() {
         this.mqttClient = this.mqttClient();
-        this.previousState = {};
-        this.clearTimeInterval = false;
-        this.timeInterval = null;
-
-        this.requestSmartBlindState();
+        this.mqttConnected = false;
+        this.mqttPreviousState = {};
+        this.blindStateInterval = null;
+        this.blindStateFuncCount = 0;
+        this.blindPollInterval = null;
     }
 
     mqttClient() {
         const mqttOptions = {
             host: options.mqtt[0].server,
             port: options.mqtt[0].port,
-            username: options.mqtt[0].user,
-            password: options.mqtt[0].passwd,
+            username: options.mqtt[0].user || null,
+            password: options.mqtt[0].passwd || null,
         }
         if (options.mqtt_ssl) {
-            mqttOptions.protocol = "mqtts";
+            mqttOptions.protocol = 'mqtts';
             mqttOptions.ca = [fs.readFileSync(options.mqtt_ssl_certificate[0].ca_path)];
             mqttOptions.cert = fs.readFileSync(options.mqtt_ssl_certificate[0].cert_path);
             mqttOptions.key = fs.readFileSync(options.mqtt_ssl_certificate[0].key_path);
             mqttOptions.rejectUnauthorized = true;
         }
         const client = mqtt.connect(mqttOptions);
-        logger.info("initializing MQTT...");
+        logger.info('initializing MQTT...');
 
-        client.on("connect", () => {
-            logger.info("MQTT connection successful!");
+        client.on('connect', () => {
+            logger.info('MQTT connection successful!');
+            this.mqttConnected = true;
 
-            const topic = "easyroll/+/+/+/command";
-            logger.info("Subscribe: " + topic);
+            const topic = 'easyroll/+/+/+/command';
+            logger.info('Subscribe: ' + topic);
 
             client.subscribe(topic);
+
+            this.requestSmartBlindState();
         });
 
-        client.on("error", (err) => {
-            logger.error("MQTT connection error: " + err);
+        client.on('error', (err) => {
+            this.mqttConnected = false;
+
+            logger.error('MQTT connection error: ' + err);
         });
 
-        client.on("reconnect", () => {
-            logger.warn("MQTT connection lost. trying to reconnect...");
+        client.on('reconnect', () => {
+            logger.warn('MQTT connection lost. trying to reconnect...');
         });
 
-        client.on("message", this.mqttCommand.bind(this));
+        client.on('message', this.mqttCommand.bind(this));
 
         return client;
     }
@@ -75,17 +80,14 @@ class EasyrollBlind {
     }
 
     async requestSmartBlindState() {
-        const self = this;
-        async function makeRequest(url, smartBlindId) {
+        const makeRequest = async (url, smartBlindId) => {
+            this.blindStateFuncCount++;
             try {
                 const response = await axios.get(url);
 
-                self.handleResponse("initial", response.data, smartBlindId);
-                setInterval(() => {
-                    self.handleResponse(null, response.data, smartBlindId);
-                }, options.scan_interval * 1000);
+                this.handleResponse(this.blindStateFuncCount, response.data, smartBlindId);
             } catch (error) {
-                logger.error(`The smart blind seems to be offline. Please check the blinds [Error: ${error}]`);
+                logger.error(`The smart blind seems to be offline. Please check the blinds [${error}]`);
             };
         }
 
@@ -93,12 +95,22 @@ class EasyrollBlind {
 
         for (const smartBlind of smartBlinds) {
             const { id, address } = smartBlind;
-            await makeRequest(stateUrl.replace("{}", address), id);
+            await makeRequest(stateUrl.replace('{}', address), id);
+        }
+    }
+
+    startSmartBlindStateRequests() {
+        this.blindStateInterval = setInterval(async () => {
+            await this.requestSmartBlindState();
+        }, options.scan_interval * 1000);
+
+        if (this.mqttConnected === false) {
+            clearInterval(this.blindStateInterval);
         }
     }
 
     handleResponse(timeInterval, body, smartBlindId) {
-        if (body.result !== "success") {
+        if (body.result !== 'success') {
             logger.error(`Smart blind (${smartBlindId}) latest status inquiry error [Result: ${body.result}]`);
             return;
         }
@@ -110,22 +122,22 @@ class EasyrollBlind {
             position: Math.round(body.position),
         };
 
-        if (timeInterval === "initial") {
+        if (timeInterval === 1) {
             logger.info(`Smart blind (${smartBlindId}) latest status inquiry success! [${body.serial_number}:${body.local_ip}]`);
+            this.startSmartBlindStateRequests();
             this.discoverSmartBlind(smartBlindState);
-        } else {
+        } else if (timeInterval > 1) {
             logger.info(`Update smart blind (${smartBlindId}) position: ${smartBlindState.position}%`);
         }
 
-        this.parseSmartBlindState(undefined, smartBlindState);
+        this.parseSmartBlindState(smartBlindState);
     }
 
     async requestSmartBlindPoll(url, smartBlindId, target) {
-        const self = this;
         try {
             const response = await axios.get(url);
 
-            if (response.data.result !== "success") {
+            if (response.data.result !== 'success') {
                 logger.error(`Smart blind (${smartBlindId}) latest status inquiry error [Result: ${response.data.result}]`);
                 return;
             }
@@ -135,70 +147,69 @@ class EasyrollBlind {
                 index: smartBlindId,
                 ip: response.data.local_ip,
                 position: Math.round(response.data.position),
-                property: ["OPEN", "CLOSE", "STOP"].includes(target) ? Math.round(response.data.position) !== 0 && Math.round(response.data.position) !== 100 : Math.round(response.data.position) != target
+                property: ['OPEN', 'CLOSE', 'STOP'].includes(target) ? Math.round(response.data.position) !== 0 && Math.round(response.data.position) !== 100 : Math.round(response.data.position) != target,
             };
-            self.parseSmartBlindState(self.mqttDict, smartBlindState);
+            this.parseSmartBlindState(smartBlindState, this.mqttStateDict);
 
             if (smartBlindState.position == target ||
-                smartBlindState.position === (target === "OPEN" ? 0 : 100) ||
-                target === "STOP"
+                smartBlindState.position === (target === 'OPEN' ? 0 : 100) ||
+                target === 'STOP'
             ) {
-                clearInterval(self.timeInterval);
+                clearInterval(this.blindPollInterval);
             }
         } catch (error) {
-            logger.error(`Failed to polling smart blind: ${smartBlindId} [Error: ${error}]`);
+            logger.error(`Failed to polling smart blind: ${smartBlindId} [${error}]`);
         }
     }
 
     async sendSmartBlindCommand(url, data, header, smartBlindId, target) {
-        const self = this;
         try {
             const response = await axios.post(url, data, header);
 
-            if (response.data.result !== "success") {
-                logger.error(`Smart blind (${smartBlindId}) ${url.body.mode === "general" ? "general operation command" : "percent move command"} error [Result: ${response.data.result}]`);
+            if (response.data.result !== 'success') {
+                logger.error(`Smart blind (${smartBlindId}) ${url.body.mode === 'general' ? 'general operation command' : 'percent move command'} error [Result: ${response.data.result}]`);
                 return;
             }
 
-            if (target !== null) {
-                self.timeInterval = setInterval(async () => {
-                    await self.requestSmartBlindPoll(url.replace("action", "lstinfo"), smartBlindId, target);
+            if (target) {
+                this.blindPollInterval = setInterval(async () => {
+                    await this.requestSmartBlindPoll(url.replace('action', 'lstinfo'), smartBlindId, target);
                 }, 1000);
             }
         } catch (error) {
-            logger.error(`Failed to ${url.body.mode === "general" ? "general operation command" : "percent move command"} smart blind: ${smartBlindId} [Error: ${error}]`);
+            logger.error(`Failed to ${url.body.mode === 'general' ? 'general operation command' : 'percent move command'} smart blind: ${smartBlindId} [${error}]`);
         }
     }
 
-    parseSmartBlindState(direction, smartBlindState) {
+    parseSmartBlindState(smartBlindState, direction) {
         if (direction && smartBlindState.property) {
-            if (direction === "CLOSE") {
-                this.direction = "closing";
-            } else if (direction === "OPEN") {
-                this.direction = "opening";
-            } else if (direction === "STOP") {
-                this.direction = "stopped";
+            if (direction === 'CLOSE') {
+                this.blindDirection = 'closing';
+            } else if (direction === 'OPEN') {
+                this.blindDirection = 'opening';
+            } else if (direction === 'STOP') {
+                this.blindDirection = 'stopped';
             }
         } else {
             if (smartBlindState.position === 0 || smartBlindState.position < 100) {
-                this.direction = "open";
+                this.blindDirection = 'open';
             } else if (smartBlindState.position === 100) {
-                this.direction = "closed";
+                this.blindDirection = 'closed';
             }
         }
 
-        if (this.previousState.serialNumber === smartBlindState.serialNumber &&
-            this.previousState.position === smartBlindState.position) {
+        if (this.mqttPreviousState.serialNumber === smartBlindState.serialNumber &&
+            this.mqttPreviousState.position === smartBlindState.position) {
             return;
         }
-        this.previousState = smartBlindState;
+        this.mqttPreviousState = smartBlindState;
 
         this.updateSmartBlind(smartBlindState);
     }
 
     updateSmartBlind(smartBlind) {
         const topics = {
-            [`easyroll/${smartBlind.index}/${smartBlind.serialNumber}/direction/state`]: this.direction,
+            [`easyroll/${smartBlind.index}/${smartBlind.serialNumber}/direction/state`]: this.blindDirection,
             [`easyroll/${smartBlind.index}/${smartBlind.serialNumber}/position/state`]: smartBlind.position.toString(),
         };
 
@@ -218,67 +229,71 @@ class EasyrollBlind {
             set_pos_t: `easyroll/${smartBlind.index}/${smartBlind.serialNumber}/position/command`,
             pos_open: 0,
             pos_clsd: 100,
+            ic: 'mdi:blinds',
             uniq_id: `easyroll_${smartBlind.serialNumber}`,
+            ret: true,
             device: {
-                ids: "Smart Blind",
-                name: "Smart Blind",
-                mf: "Easyroll",
-                mdl: "Easyroll Inshade",
-                sw: "harwin1/ha-addons/easyroll_blind",
+                ids: 'Smart Blind',
+                name: 'Smart Blind',
+                mf: 'EasyRoll',
+                mdl: 'EasyRoll Inoshade',
+                sw: 'harwin1/ha-addons/easyroll_blind',
             },
         };
-        this.mqttClient.publish(topic, JSON.stringify(payload), { retain: true });
+        this.mqttClient.publish(topic, JSON.stringify(payload));
 
-        for (const key of ["M1", "M2", "M3"]) {
+        for (const key of ['M1', 'M2', 'M3']) {
             const topic = `homeassistant/button/easyroll_${smartBlind.index}_${key}/${smartBlind.serialNumber}/config`;
             const payload = {
                 name: `easyroll_${smartBlind.serialNumber}_${key}`,
                 cmd_t: `easyroll/${smartBlind.index}/${smartBlind.serialNumber}/${key}/command`,
                 uniq_id: `easyroll_${smartBlind.serialNumber}_${key}`,
+                ret: true,
+                ic: 'mdi:alpha-m-box',
                 device: {
-                    ids: "Smart Blind",
-                    name: "Smart Blind",
-                    mf: "Easyroll",
-                    mdl: "Easyroll Inshade",
-                    sw: "harwin1/ha-addons/easyroll_blind"
-                }
+                    ids: 'Smart Blind',
+                    name: 'Smart Blind',
+                    mf: 'EasyRoll',
+                    mdl: 'EasyRoll Inoshade',
+                    sw: 'harwin1/ha-addons/easyroll_blind',
+                },
             };
-            this.mqttClient.publish(topic, JSON.stringify(payload), { retain: true });
+            this.mqttClient.publish(topic, JSON.stringify(payload));
         }
     }
 
     mqttCommand(topic, message) {
-        const topics = topic.split("/");
+        const topics = topic.split('/');
         const payload = message.toString();
         const hosts = this.findSmartBlinds();
         const hostDict = [];
 
-        if (topics[0] !== "easyroll") {
-            logger.error("Invalid topic prefix: " + topics[0]);
+        if (topics[0] !== 'easyroll') {
+            logger.error('Invalid topic prefix: ' + topics[0]);
             return;
         }
 
         for (const host of hosts) {
             const { id, address } = host;
             if (id == topics[1]) {
-                hostDict.push(id, actionUrl.replace("{}", address));
+                hostDict.push(id, actionUrl.replace('{}', address));
             }
         }
         logger.info(`Received message: ${topic} = ${payload}`);
 
-        if (topics[3] === "direction") {
-            this.mqttDict = payload;
-            this.sendSmartBlindCommand(hostDict[1], { "mode": "general", "command": command[payload] }, { headers: { "content-type": "application/json" } }
+        if (topics[3] === 'direction') {
+            this.mqttStateDict = payload;
+            this.sendSmartBlindCommand(hostDict[1], { 'mode': 'general', 'command': command[payload] }, { headers: { 'content-type': 'application/json' } }
                 , hostDict[0], payload);
-        } else if (topics[3] === "position") {
-            this.mqttDict = (payload < this.previousState.position ? "OPEN" : "CLOSE");
-            this.sendSmartBlindCommand(hostDict[1], { "mode": "level", "command": payload }, { headers: { "content-type": "application/json" } }
+        } else if (topics[3] === 'position') {
+            this.mqttStateDict = (payload < this.mqttPreviousState.position ? 'OPEN' : 'CLOSE');
+            this.sendSmartBlindCommand(hostDict[1], { 'mode': 'level', 'command': payload }, { headers: { 'content-type': 'application/json' } }
                 , hostDict[0], payload);
         } else {
-            this.sendSmartBlindCommand(hostDict[1], { "mode": "general", "command": topics[3] }, { headers: { "content-type": "application/json" } }
-                , hostDict[0], null);
+            this.sendSmartBlindCommand(hostDict[1], { 'mode': 'general', 'command': topics[3] }, { headers: { 'content-type': 'application/json' } }
+                , hostDict[0]);
         }
     }
 }
 
-new EasyrollBlind();
+new SmartBlind();
