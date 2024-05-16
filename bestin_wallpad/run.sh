7bffbbd170095689781fe944a4ab648bcfa38df4
lunDreame <87955512+lunDreame@users.noreply.github.com>
2024-05-16 11:10:55 +0900

    Update run.sh

--- bestin_wallpad/run.sh
+++ bestin_wallpad/run.sh
@@ -1,36 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
 share_dir=/share/bestin
 srv_dir=$share_dir/srv
 
-create_dir_if_not_exists() {
-    if [ ! -d "$1" ]; then
-        mkdir -p "$1"
-        echo "INFO: Created directory '$1'."
-    fi
-}
-
 copy_file() {
-    if [ -f "/$1" ]; then
-        cp -f "/$1" "$2"
-        echo "INFO: Copied '$1' to '$2'."
-    else
-        echo "WARNING: File '$1' not found."
+    if [ ! -f "$share_dir/$1" ]; then
+        mkdir -p "$share_dir"
+        mv "/$1" "$share_dir"
     fi
 }
 
-create_dir_if_not_exists "$share_dir"
-create_dir_if_not_exists "$srv_dir"
+copy_file_to_srv() {
+    if [ ! -f "$srv_dir/$1" ]; then
+        mkdir -p "$srv_dir"
+        mv "/$1" "$srv_dir"
+    fi
+}
 
-copy_file "bestin.js" "$share_dir"
-copy_file "const.js" "$srv_dir"
-copy_file "logger.js" "$srv_dir"
-
-if [ -f "$share_dir/bestin.js" ]; then
-    echo "INFO: 'bestin.js' copied successfully."
+run_node() {
     echo "INFO: Starting HDC BESTIN Wallpad Add-on..."
     cd "$share_dir"
     node bestin.js
+}
+
+copy_file "bestin.js"
+copy_file_to_srv "const.js"
+copy_file_to_srv "logger.js"
+
+if [ -f "$share_dir/bestin.js" ]; then
+    run_node
 else
     echo "ERROR: Failed to copy 'bestin.js' to $share_dir"
     exit 1
